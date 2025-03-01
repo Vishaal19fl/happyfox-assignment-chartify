10f66aa5461f20cc5729d077850645f8565fe618
innovate444444 <vishaalkrish6@gmail.com>
2025-03-02 00:40:16 +0530

    landing section

---
+++ client/src/pages/landing/Landing.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './Landing.scss'
+import { AiOutlineSearch } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+
+export const Landing = () => {
+  return (
+    <div className='hero'>
+      <div className='container'>
+        <div className='content'>
+          <p className='subtitle'>Chart Your Organization</p>
+          <h1 className='title'>
+    Build & Manage <span>Team Structures</span> with{' '}
+    <span>Drag-and-Drop</span> Ease
+</h1>
+<p className='description'>
+    Create, visualize, and update your organization chart effortlessly. Empower your team with a clear and dynamic hierarchy.
+</p>
+
+          <button className='get-started-btn'>
+          <Link to="/chart-page" className='link'>
+            Get Started </Link>
+          </button>
+          
+        </div>
+
+        <div className='image'>
+          <img src="/img/team.png" alt="Hero" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
