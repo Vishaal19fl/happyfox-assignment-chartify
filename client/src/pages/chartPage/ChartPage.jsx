4ecdec2df5075e8c817c1eaa88fbd25bbee8d3db
innovate444444 <vishaalkrish6@gmail.com>
2025-03-03 13:08:40 +0530

    change

--- client/src/pages/chartPage/ChartPage.jsx
+++ client/src/pages/chartPage/ChartPage.jsx
@@ -14,19 +14,20 @@ import Pretender from 'pretender';
 const server = new Pretender(function() {
   
   const employeeData = [
-    { id: 1, name: "Mark Hill", designation: "CEO", team: "Executive", managerId: null, image: "/img/m1.png" },
-    { id: 2, name: "Joe Linux", designation: "CTO", team: "Executive", managerId: 1, image: "/img/m2.png" },
-    { id: 3, name: "Linda May", designation: "CFO", team: "Finance", managerId: 1, image: "/img/w1.png" },
-    { id: 4, name: "John Green", designation: "VP of Engineering", team: "Engineering", managerId: 2, image: "/img/m3.png" },
-    { id: 5, name: "Ron Blomquist", designation: "VP of IT", team: "IT", managerId: 2, image: "/img/w2.png" },
-    { id: 6, name: "Michael Rubin", designation: "VP of Finance", team: "Finance", managerId: 3, image: "/img/m4.png" },
-    { id: 7, name: "Alice Johnson", designation: "Engineering Manager", team: "Engineering", managerId: 4, image: "/img/m5.png" },
-    { id: 8, name: "Bob Williams", designation: "IT Manager", team: "IT", managerId: 5, image: "/img/w3.png" },
-    { id: 9, name: "Carol Smith", designation: "Financial Analyst", team: "Finance", managerId: 6, image: "/img/m6.png" },
-    { id: 10, name: "Dave Brown", designation: "Software Engineer", team: "Engineering", managerId: 7, image: "/img/w4.png" },
-    { id: 11, name: "Eve Davis", designation: "System Administrator", team: "IT", managerId: 8, image: "/img/m7.png" },
-    { id: 12, name: "Frank Wilson", designation: "Accountant", team: "Finance", managerId: 9, image: "/img/w5.png" },
-  ];
+    { id: 1, name: "Vishaal Krishna", designation: "CEO", team: "Executive", managerId: null, image: "/img/m1.png" },
+    { id: 2, name: "Arun Kumar", designation: "CTO", team: "Executive", managerId: 1, image: "/img/m2.png" },
+    { id: 3, name: "Priya Ramesh", designation: "CFO", team: "Finance", managerId: 1, image: "/img/w1.png" },
+    { id: 4, name: "Vikram Patel", designation: "VP of Engineering", team: "Engineering", managerId: 2, image: "/img/m3.png" },
+    { id: 5, name: "Sunita Verma", designation: "VP of IT", team: "IT", managerId: 2, image: "/img/w2.png" },
+    { id: 6, name: "Manoj Sekar", designation: "VP of Finance", team: "Finance", managerId: 3, image: "/img/m4.png" },
+    { id: 7, name: "Anjali", designation: "Engineering Manager", team: "Engineering", managerId: 4, image: "/img/m5.png" },
+    { id: 8, name: "Ramesh Yadav", designation: "IT Manager", team: "IT", managerId: 5, image: "/img/w3.png" },
+    { id: 9, name: "Kavita", designation: "Financial Analyst", team: "Finance", managerId: 6, image: "/img/m6.png" },
+    { id: 10, name: "Suresh Kumar", designation: "Software Engineer", team: "Engineering", managerId: 7, image: "/img/w4.png" },
+    { id: 11, name: "Neha Sri", designation: "System Administrator", team: "IT", managerId: 8, image: "/img/m7.png" },
+    { id: 12, name: "Mohammed Shakithiyan", designation: "Accountant", team: "Finance", managerId: 9, image: "/img/w5.png" },
+];
+
 
   // GET endpoint to fetch all employees
   this.get('/api/employees', () => {
